669d8b98af3ed237c514abe889df6b2396fb2def
Mahmoud <90093238+Damahmoud@users.noreply.github.com>
2023-11-17 16:18:57 +0200

    Add files via upload

---
+++ sand.js
@@ -0,0 +1,45 @@
+// const btn = document.getElementById('btn');
+// const ul = document.getElementById('ul');
+// const dot = document.getElementById('dot');
+
+// btn.onclick = function(){
+//     ul.style.display = 'block';
+//     dot.style.display = 'block';
+// }
+
+// dot.onclick = function(){
+//     ul.style.display = 'none';
+//     dot.style.display = 'none';
+// }
+
+
+const show1 = document.getElementById('show1');
+const box1 = document.getElementById('click1');
+function sam(){
+    show1.classList.toggle('m');
+    box1.classList.toggle('zht');
+}
+
+
+const show2 = document.getElementById('show2');
+const box2 = document.getElementById('click2');
+function nam(){
+    show2.classList.toggle('m');
+    box2.classList.toggle('zht');
+}
+
+
+const show3 = document.getElementById('show3');
+const box3 = document.getElementById('click3');
+function mam(){
+    show3.classList.toggle('m');
+    box3.classList.toggle('zht');
+}
+
+
+
+
+
+
+
+
